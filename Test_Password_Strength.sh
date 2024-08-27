6dfb70a34dd062ad9e2c920ab911d2e446a04921
Tim Whelan <78320738+twhelan25@users.noreply.github.com>
2024-08-27 09:41:03 -0600

    Update Test_Password_Strength.sh

--- Test_Password_Strength.sh
+++ Test_Password_Strength.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 read -s -p "Please create your password. Your password should use a combination of uppercase and lowercase letters, numbers, and special characters. Please input your password: " password
-if [[ $password =~ ^[A-Za-z0-9!@\#$%^&*()_+={}:;|<>,.?/-]{8,}$ ]]; then
-  echo "Your password is strong!"
+echo # Move to new line after password input
+
+if [[ ${#password} -ge 8 && "$password" =~ [A-Z] && "$password" =~ [a-z] && "$password" =~ [0-9] && "$password" =~ [!@#$%^&*()_+={}:;|<>,.?/-] ]]; then
+	echo "Your password is strong!"
 else
-  echo "Your password is weak! Try using a combination of uppercase and lowercase letters, numbers, and special characters."
+	echo "Your password is weak! Try using a combination of uppercase and lowercase letters, numbers, and special characters."
 fi
-  
